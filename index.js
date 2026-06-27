918e97880f9e2fa31b0d14f72ebeaa694e75a16c
BhagirathsinhET <bhagirathn.encircle@gmail.com>
2026-06-27 14:08:33 +0530

    feat:demo Project

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Node CI/CD Demo Running 🚀",
+  });
+});
+app.get("/health", (req, res) => {
+  res.json({
+    success: true,
+    message: "Health Route Visit Demo Running 🚀",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
